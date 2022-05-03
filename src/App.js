758840513abe3fcb846571f64f639974d17ee8e8
Zohaib <54810131+mirzazohaib@users.noreply.github.com>
2022-05-03 10:35:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Dashboard from "./Dashboard";
+import Course from "./Course";
+import Programme from "./Programme";
+import { BrowserRouter as Routes, Route, Link, Router } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/Course">Course</Link>
+            </li>
+            <li>
+              <Link to="/Programme">Programme</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/Course" element={<Course />} />
+          <Route path="/Programme" element={<Programme />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
